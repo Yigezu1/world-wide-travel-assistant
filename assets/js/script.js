fddb4122ca54b780c02f6f2ab31b5ceafe95ae97
Mongolmaa Gurbazar <mongolmaa555@gmail.com>
2020-09-24 12:12:20 -0400

    push before pull

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 $.ajax({
-    // url:"https://calendarific.com/api/v2/holidays?&api_key=dfd7976d8e75420dd3c5e1009a633b9446657f23&country=ET&year=2020&month=1,2",
-    method: "GET"
-}).then(function(response){
-console.log(response);
+  url:
+    "https://calendarific.com/api/v2/holidays?&api_key=dfd7976d8e75420dd3c5e1009a633b9446657f23&country=ET&year=2020&month=1,2",
+  method: "GET",
+}).then(function (response) {
+  console.log(response);
 });
+//&country=ET&year=2020&month=1,2
